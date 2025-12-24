e1b56b71940ca47c9b6a7fd36922d54529a2d3ee
mmahmoudramez0 <mahmoudramez0@gmail.com>
2025-12-24 09:18:46 +0200

    put anything in the email

--- test/test_main.py
+++ test/test_main.py
@@ -40,7 +40,7 @@ class Test_Login:
             EC.visibility_of_element_located((By.CSS_SELECTOR, "input[type='email']"))
         )
         email_input.clear()
-        email_input.send_keys("")
+        email_input.send_keys("asdasdasda")
 
 
         success_indicator = (By.CSS_SELECTOR, "div.go2072408551")
